7a1615a31bc81f7de9ec89ec580412cc254f2710
Adon Moskal <adonm@op.ac.nz>
2023-08-09 09:36:58 +1200

    ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,29 @@
-<h1>Hello, welcome to my site!</h1>
+<h1 id="firstTitle">Hello, welcome to my site!</h1>
+
+<h1 id="secondTitle">Section 2</h1>
+
+<div class="boldText">
+    Div
+</div>
+
+<p class="boldText">
+    Paragraph
+</p>
+
+<a href="#" class="boldText">
+    I am a link
+</a>
+
+<div>
+    <div id="myId">
+        <div>
+            <div>
+                <a href="#" class="boldText">
+                    <div>
+                        <div class="anotherClass">Hello</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
